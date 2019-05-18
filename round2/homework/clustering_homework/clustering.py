6b241a121108cc874d4333cced7cf300b789e0e0
tigofat <aldenold10@gmail.com>
2019-05-19 02:39:28 +0400

    Complete k_means and do clustering with images.

--- round2/homework/clustering_homework/clustering.py
+++ round2/homework/clustering_homework/clustering.py
@@ -8,7 +8,11 @@ class MeasureFunctions:
 	@staticmethod
 	def distances_measure(x, x_):
 		dist = (x - x_) ** 2
-		return np.sqrt(np.abs(np.sum(dist, axis=1)))
+		return np.sqrt(np.abs(np.sum(dist, axis=0)))
+
+	@staticmethod
+	def similar_color(c, c_):
+		return np.sum(np.abs(c - c_) ** 2)
 
 
 @dataclass(init=True, repr=True)
@@ -19,18 +23,23 @@ class k_means:
 	n_clusters: int
 	# similarity measure function
 	simil_meas_func: object
+	similar_loss_value: float = 1000
+	n_similar_loss: int = 3
+	sample_nks: np.ndarray = None
+	sample_nk_limits: tuple = ()
+	mean_func: object = lambda x: x.mean(axis=0)
 
-	def train(self, data, sample_nks=None, sample_nk_limits=None):
+	def train(self, data):
 
-		self.loss = []
+		self.loss_history = []
 		self.r = np.zeros((len(data), self.n_clusters))
 
-		if sample_nk_limits:
-			nk_limits = sample_nk_limints
+		if self.sample_nk_limits:
+			nk_limits = self.sample_nk_limints
 		else: nk_limits = data.min(), data.max()
 
-		self.nk = (sample_nks if sample_nks 
-				else np.random.uniform(0, 30, 
+		self.nk = (self.sample_nks if not self.sample_nks is None
+				else np.random.uniform(*nk_limits, 
 									size=(self.n_clusters, 
 									data.shape[1])))
 		self.EM(data)
@@ -38,26 +47,61 @@ class k_means:
 		return self
 
 	def EM(self, data):
-		for _ in range(5):
-			self.__expectation(data)
-			self.__minimization(data)
 
-	def predict(self, test_data):
-		pass
+		while True:
+			self.r[:] = self.__expectation(data)
+			self.nk[:] = self.__minimization(data)
+
+			current_loss = self.loss(data)
+			last_n_losses = self.loss_history[-self.n_similar_loss:]
+			self.loss_history.append(current_loss)
+
+			if len(self.loss_history) < self.n_similar_loss:
+				continue
+
+			if np.all(last_n_losses 
+					  - current_loss <= self.similar_loss_value):
+				break
+
+	def predict(self, data):
+		return self.__expectation(data)
 
 	def loss(self, data):
-		for n in data
+		loss = 0
+		for n in range(len(data)):
+			for k in range(len(self.nk)):
+				loss += self.r[n][k] * self.simil_meas_func(data[n], 
+															self.nk[k])
+		return loss
 
 	def __expectation(self, data):
-		rns = np.array([self.simil_meas_func(x, self.nk) for x in data])
+		rns = np.array([
+				[self.simil_meas_func(x, k) for k in self.nk]
+				for x in data
+		])
 		minim_dis_indices = np.argmin(rns, axis=1)
-		self.r[:] = 0
+
+		r = np.zeros(self.r.shape)
+		r[:] = 0
+
 		for i in range(len(minim_dis_indices)):
-			self.r[i, minim_dis_indices[i]] = 1
+			r[i, minim_dis_indices[i]] = 1
+		return r
 
 	def __minimization(self, data):
 		binary_columns = self.r.T
-		k_cluster_xs = [np.where(column == 1) 
-						for column in binary_columns]
-		for i in range(len(self.nk)):
-			self.nk[i] = data[k_cluster_xs[i]].mean(axis=0)
+		k_cluster_xs = [
+				np.where(column == 1) 
+				for column in binary_columns 
+		]
+
+		nk = np.zeros(self.nk.shape)
+
+		for i in range(len(nk)):
+			# some clusters might not have any points
+			if data[k_cluster_xs[i]].size == 0:
+				continue
+
+			nk[i] = self.mean_func(data[k_cluster_xs[i]])
+
+		return nk
